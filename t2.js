3df0617831c5b8310fbf32847968a280cf6de11d
Crazy4Redstone <mcpe1398@gmail.com>
2014-02-28 19:47:03 -0600

    Create t2.js

---
+++ t2.js
@@ -0,0 +1,27 @@
+Block.defineBlock (77,"Stone Button",[["stone",0]],50,false,7)
+Block.setShape (77,0,0.38,0.31,0.12,0.62,0.69)
+Block.setRenderLayer (77,1)
+
+Block.defineBlock (100,"Stone Button",[["stone",0]],50,false,7)
+Block.setShape (100,0.12,0.38,0.31,0,0.62,0.69)
+Block.setRenderLayer (100,1)
+
+function useItem (x,y,z,itemId,blockId,side)
+{
+  if (itemId == 77 && side == 2)
+  {
+    setTile (x-5,y,z,77)
+  }
+}
+
+function procCmd (c) {
+    var p = c.split(" ");
+    var command = p[0];
+    switch(command) {
+        case 'give': {
+            if(p[1] > 0) {
+                Entity.setCarriedItem(getPlayerEnt(),p[1],1,p[2]);
+            }
+        }
+    }
+}
