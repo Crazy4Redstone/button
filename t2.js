c3240614cc913be5c399929916d1d5a744126cfe
Crazy4Redstone <mcpe1398@gmail.com>
2014-02-28 20:02:14 -0600

    Update t2.js

--- t2.js
+++ t2.js
@@ -3,7 +3,7 @@ Block.setShape (77,0,0.38,0.31,0.12,0.62,0.69)
 Block.setRenderLayer (77,1)
 
 Block.defineBlock (100,"Stone Button",[["stone",0]],50,false,7)
-Block.setShape (100,0.12,0.38,0.31,0,0.62,0.69)
+Block.setShape (100,0.88,0.38,0.31,1,0.62,0.69)
 Block.setRenderLayer (100,2)
 
 function useItem (x,y,z,itemId,blockId,side)
