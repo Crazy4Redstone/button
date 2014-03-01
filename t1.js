d7c724a3b44094fda6b5b01e1db80eaf880b9b58
Crazy4Redstone <mcpe1398@gmail.com>
2014-02-28 18:40:09 -0600

    Update t1.js

--- t1.js
+++ t1.js
@@ -1,5 +1,5 @@
 Block.defineBlock (77,"Stone Button",[["stone",0]],50,false,7)
-Block.setShape (77,0.25,11/16,0.25,0.25,6/16,0.25)
+Block.setShape (77,0,0,0,1,1,1)
 Block.setRenderLayer (77,2)
 
 function useItem (x,y,z,b,i,side)
