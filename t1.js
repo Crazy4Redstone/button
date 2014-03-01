b90bf6ce7ed680c4e49b4898fbc31e407f00f399
Crazy4Redstone <mcpe1398@gmail.com>
2014-02-28 19:38:04 -0600

    Update t1.js

--- t1.js
+++ t1.js
@@ -2,9 +2,9 @@ Block.defineBlock (77,"Stone Button",[["stone",0]],50,false,7)
 Block.setShape (77,0,0.38,0.31,0.12,0.62,0.69)
 Block.setRenderLayer (77,1)
 
-function useItem (x,y,z,b,i,side)
+function useItem (x,y,z,itemId,blockId,side)
 {
-  if (b !==0 && i == 77 && side == 3)
+  if (itemId == 77 && side == 3)
   {
     setTile (x-5,y,z,77)
   }
